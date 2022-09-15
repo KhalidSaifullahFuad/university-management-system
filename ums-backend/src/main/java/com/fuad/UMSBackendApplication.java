846d281fd1c9bdfd610f733e57605b620b3e46f1
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-09-15 18:18:43 +0600

    🚀 Initialized project with backend module

---
+++ ums-backend/src/main/java/com/fuad/UMSBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fuad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMSBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UMSBackendApplication.class, args);
+    }
+
+}
---
+++ ums-backend/src/test/java/com/fuad/UMSBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMSBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
