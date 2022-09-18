54f6d3d21936e435d7b8ccd61fc050aeaa752be3
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-09-18 23:46:48 +0600

    ✅ Added Spring Fox Swagger

--- ums-backend/src/main/java/com/fuad/UMSBackendApplication.java
+++ ums-backend/src/main/java/com/fuad/UMSBackendApplication.java
@@ -1,9 +1,12 @@
 package com.fuad;
 
+import com.fuad.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class UMSBackendApplication {
 
     public static void main(String[] args) {
